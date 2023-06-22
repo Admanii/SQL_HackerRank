b71e79c2ae392322b32b91da4c2dfdbd02ba4645
Ahmed Admani <ahmedriaz-50@hotmail.com>
2023-06-22 22:15:14 +0500

    THE REPORT - BASIC JOIN

---
+++ BASIC JOINS/TheReport.sql
@@ -0,0 +1,9 @@
+SELECT 
+CASE 
+WHEN grades.grade < 8 THEN NULL
+WHEN grades.grade >= 8 THEN students.NAME END,  
+Grades.grade , students.MARKS
+from students 
+INNER JOIN Grades 
+WHERE students.marks >= Grades.min_mark and students.marks <= Grades.max_mark
+ORDER BY Grades.GRADE DESC, STUDENTS.NAME ASC;
