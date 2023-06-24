6a1b27af4c6c5bb960b3375940b86ffa436c3d2c
Ahmed Admani <ahmedriaz-50@hotmail.com>
2023-06-24 19:43:04 +0500

    Olivander Inventory Joins/SubQuerry

---
+++ BASIC JOINS/OllivanderInventory.sql
@@ -0,0 +1,23 @@
+SELECT
+  w.id,
+  wp.age,
+  w.coins_needed,
+  w.power
+FROM
+  wands w
+  JOIN Wands_Property wp ON w.code = wp.code
+WHERE
+  w.coins_needed = (
+    SELECT
+      MIN(coins_needed)
+    FROM
+      wands w2
+      JOIN Wands_Property wp2 ON w2.code = wp2.code
+    WHERE
+      wp2.is_evil = 0
+      AND w2.power = w.power
+      AND wp2.age = wp.age
+  )
+ORDER BY
+  w.power DESC,
+  wp.age DESC;
