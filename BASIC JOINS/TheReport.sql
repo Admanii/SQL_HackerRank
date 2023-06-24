d5c29c8c48e24372225dd6fe901f8724cfce7a86
Ahmed Admani <ahmedriaz-50@hotmail.com>
2023-06-24 19:44:16 +0500

    Formatting

--- BASIC JOINS/TheReport.sql
+++ BASIC JOINS/TheReport.sql
@@ -1,9 +1,16 @@
-SELECT 
-CASE 
-WHEN grades.grade < 8 THEN NULL
-WHEN grades.grade >= 8 THEN students.NAME END,  
-Grades.grade , students.MARKS
-from students 
-INNER JOIN Grades 
-WHERE students.marks >= Grades.min_mark and students.marks <= Grades.max_mark
-ORDER BY Grades.GRADE DESC, STUDENTS.NAME ASC;
+SELECT
+  CASE
+    WHEN grades.grade < 8 THEN NULL
+    WHEN grades.grade >= 8 THEN students.NAME
+  END,
+  Grades.grade,
+  students.MARKS
+from
+  students
+  INNER JOIN Grades
+WHERE
+  students.marks >= Grades.min_mark
+  and students.marks <= Grades.max_mark
+ORDER BY
+  Grades.GRADE DESC,
+  STUDENTS.NAME ASC;
--- BASIC JOINS/TopCompetitors.sql
+++ BASIC JOINS/TopCompetitors.sql
@@ -1,10 +1,21 @@
-SELECT s.hacker_id, h.name
-FROM hackers h
-RIGHT JOIN submissions s ON h.hacker_id = s.hacker_id
-LEFT JOIN challenges c ON c.challenge_id = s.challenge_id
-LEFT JOIN Difficulty d ON d.difficulty_level = c.difficulty_level
-WHERE d.score = s.score
-GROUP BY s.hacker_id, h.name
-HAVING COUNT(h.name) > 1
-ORDER BY COUNT(h.name) DESC, s.hacker_id ASC;
+SELECT
+  s.hacker_id,
+  h.name
+FROM
+  hackers h
+  RIGHT JOIN submissions s ON h.hacker_id = s.hacker_id
+  LEFT JOIN challenges c ON c.challenge_id = s.challenge_id
+  LEFT JOIN Difficulty d ON d.difficulty_level = c.difficulty_level
+WHERE
+  d.score = s.score
+GROUP BY
+  s.hacker_id,
+  h.name
+HAVING
+  COUNT(h.name) > 1
+ORDER BY
+  COUNT(h.name) DESC,
+  s.hacker_id ASC;
+
+
 -- Apparently works with inner join too xD
